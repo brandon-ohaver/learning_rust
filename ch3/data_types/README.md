c33bb73fc06a7661a694885e6e7ef02be1b71df9
brandon-ohaver <brandon.ohaver@gmail.com>
2024-07-08 14:01:12 -0500

    Finished Chapter 3.2

---
+++ ch3/data_types/README.md
@@ -0,0 +1,13 @@
+# Chapter 3 Section 2: Data Types
+
+## Every value in Rust is a certain data type, which tells Rust waht kind of data is being specified so it knows how to work with that data. In this section, two data types subsets are described: Scalar and Compound
+
+#### Note: Rust is a statically typed language, which means Rust must know the types of all variables at compile time
+
+## Scalar Types
+
+#### A scalar type represents a single value. Rust has four primary scalar types: integers, floating-point numbers, Booleans, and characters.
+
+## Compound Types
+
+#### Compound types can group multiple values into one type. Rust has two primitive compound types: Tuples and Arrays
--- ch3/data_types/src/main.rs
+++ ch3/data_types/src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // if `u32` type notation is not used, then Rust will require it and error
+    let guess: u32 = "42".parse().expect("Not a number!");
+    // this is required due to parse() which could infer may different types
+
+
+    // when inputing a number larger than the given array, an index out of bounds error will be printed to the screen
+    let a = [1, 2, 3, 4, 5]; // This is an array
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Should be a number!");
+
+    let index: usize = index.trim().parse().expect("Not a number!");
+
+    let element = a[index];
+
+    println!("The element is {element}");
 }
