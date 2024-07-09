fb07eee5dc56cb483788e401a35635cfb064710a
brandon-ohaver <brandon.ohaver@gmail.com>
2024-07-09 09:41:38 -0500

    FInished Chapter 3.3

--- ch3/functions/README.md
+++ ch3/functions/README.md
@@ -2,4 +2,6 @@
 
 ## Functions are prevalent in Rust code. The most important function within Rust is the `main` function. The `fn` keyword is used to allow the declaration of new functions. Rust code typically uses `snake case` as the conventional style for naming functions and variables, in which letters are all lowercase and underscores separate words.
 
-## 
+## Something to remember: Statements in Rust are instructsion that perform some action and do not return a value, which means they have a semicolon at the end of the line. Expressions evaluate to a resultant value, usually with the semicolon being excluded from the end of the line so that they return a value. Adding a semicolon to the end of an expression turns it into a statement!
+
+## Also note: expressions don't require for the `return` keyword to be used, but it will be required if the function needs to return a value early from itself
--- ch3/functions/src/main.rs
+++ ch3/functions/src/main.rs
@@ -1,3 +1,33 @@
+// This is the most important function `main`
 fn main() {
     println!("Hello, world!");
+
+    // uses another_function to print to the screen
+    another_function();
+
+    // uses next_function to print the value to the screen
+    next_function(5);
+
+    // creates immutable variable x which holds the return value from add_numbers()
+    let x = add_numbers(1, 2);
+
+    // prints the value held in x
+    println!("The value of 1 and 2 together is {x}");
+}
+
+// this is an example of a simple function that takes no parameters
+fn another_function() {
+    println!("This is from another function.");
+}
+
+// this shows a function with a parameter x with type i32
+fn next_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+// This function has two parameters: x and y
+// This also has a return value of i32
+// This also shows that the `return` keyword is not necessary
+// Instead, just don't add a semicolon to the end
+fn add_numbers(x: i32, y: i32) -> i32 {
+    x + y
 }
