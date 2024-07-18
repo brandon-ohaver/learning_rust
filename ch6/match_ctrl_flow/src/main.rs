7724cd39778b028b5f59365695e9f4adfb691dbe
brandon-ohaver <brandon.ohaver@gmail.com>
2024-07-18 17:08:17 -0500

    Finished Chapter 6 Section 2

--- ch6/match_ctrl_flow/src/main.rs
+++ ch6/match_ctrl_flow/src/main.rs
@@ -1,3 +1,31 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Missouri,
+    Illinois,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin { // this allows us to unwrap the Coin enum from the value
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => { // this allows us to unwrap the state enum from the value
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    value_in_cents(Coin::Quarter(UsState::Illinois));
 }
